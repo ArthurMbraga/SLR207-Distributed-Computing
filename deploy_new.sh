ab91146982fdfe1a4005c930916eb1dd9f80705c
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-06-04 11:21:23 +0200

    Updating script

--- deploy_new.sh
+++ deploy_new.sh
@@ -17,20 +17,21 @@ nameOfTheJarToExecute="server-1-jar-with-dependencies.jar"
 computers=("tp-1a201-17" "tp-1a201-18" "tp-1a201-19")
 
 for c in "${computers[@]}"; do
+  sshpass=("sshpass" "-p" "$password")
   # Command to kill all the user processes, remove the remote folder and create a new one
-  command0=("sshpass" "-p" "$password" "ssh" "$login@$c" "lsof -ti | xargs kill -9 2>/dev/null; rm -rf $remoteFolder; mkdir $remoteFolder")
+  command0=("ssh" "$login@$c" "lsof -ti | xargs kill -9 2>/dev/null; rm -rf $remoteFolder; mkdir $remoteFolder")
   
   # Command to copy the folder to the remote folder
-  command1=("sshpass" "-p" "$password" "scp" "-r" "$localFolder$todeploy" "$login@$c:$remoteFolder")
+  command1=("scp" "-r" "$localFolder$todeploy" "$login@$c:$remoteFolder")
   
   # Command to go to the remote folder, wait 3 seconds, and execute the jar
-  command2=("sshpass" "-p" "$password" "ssh" "-tt" "$login@$c" "cd $remoteFolder; sleep 3; java -jar $remoteFolder/$nameOfTheJarToExecute")
+  command2=("ssh" "-tt" "$login@$c" "cd $remoteFolder; sleep 3; java -jar $remoteFolder/$nameOfTheJarToExecute")
 
   echo "${command0[*]}"
-  "${command0[@]}"
+  "${sshpass[@]}" "${command0[@]}"
   echo "${command1[*]}"
-  "${command1[@]}"
+  "${sshpass[@]}" "${command1[@]}"
   echo "${command2[*]}"
-  "${command2[@]}" &
+  "${sshpass[@]}" "${command2[@]}" &
   
 done
